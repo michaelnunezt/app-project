f8b46cd2f55cdba4ce32afdb9544903ecf5d8652
michaelnunezt <michaelnunezt@gmail.com>
2024-12-02 15:19:42 +0100

    images changes

--- db/data/destinations.js
+++ db/data/destinations.js
@@ -3,7 +3,7 @@ module.exports = [
     city: "Rome",
     country: "Italy",
     description: "The capital of Italy, known for its nearly 3,000 years of globally influential art, architecture, and culture.",
-    images: "/images/Rome.jpg",
+    images: "https://i.natgeofe.com/n/3012ffcc-7361-45f6-98b3-a36d4153f660/colosseum-daylight-rome-italy.jpg?w=2560&h=1706",
     pointsOfInterest: [
       { name: "Colosseum", description: "An ancient amphitheater, the largest ever built, and an iconic symbol of Roman imperial power." },
       { name: "Vatican City", description: "The independent city-state that is home to the Pope and St. Peter's Basilica." },
@@ -47,7 +47,7 @@ module.exports = [
     city: "Kerry",
     country: "Ireland",
     description: "A beautiful county in the southwest of Ireland, known for its rugged coastline, scenic mountains, and national parks.",
-    images: "/images/Kerry.png",
+    images: "https://www.irishcentral.com/uploads/article-v2/2021/10/148551/cropped_Kerry.TourismIreland.jpg?t=1633689748",
     pointsOfInterest: [
       { name: "Ring of Kerry", description: "A circular tourist route that offers stunning views of the coastline and countryside." },
       { name: "Killarney National Park", description: "A 26,000-acre park with lakes, mountains, and woodland, ideal for outdoor activities." },
@@ -58,7 +58,7 @@ module.exports = [
     city: "Sydney",
     country: "Australia",
     description: "The largest city in Australia, known for its iconic harborfront and vibrant cultural scene.",
-    images: "/images/Sydney.png",
+    images: "https://images.goway.com/production/featured_images/australia_sydney_opera_house_gt_mag.jpg",
     pointsOfInterest: [
       { name: "Sydney Opera House", description: "An architectural masterpiece and a multi-venue performing arts center." },
       { name: "Sydney Harbour Bridge", description: "A massive steel arch bridge that connects the city's north and south shores." },
@@ -69,7 +69,7 @@ module.exports = [
     city: "Kyoto",
     country: "Japan",
     description: "A city famous for its classical Buddhist temples, gardens, imperial palaces, and traditional wooden houses.",
-    images: "/images/Kyoto.png",
+    images: "https://boutiquejapan.com/wp-content/uploads/2019/07/yasaka-pagoda-higashiyama-kyoto-japan.jpg",
     pointsOfInterest: [
       { name: "Fushimi Inari Shrine", description: "A famous Shinto shrine known for its thousands of vermilion torii gates." },
       { name: "Kinkaku-ji", description: "The Golden Pavilion, a Zen Buddhist temple covered in gold leaf." },
@@ -80,7 +80,7 @@ module.exports = [
     city: "Cape Town",
     country: "South Africa",
     description: "A port city on South Africa’s southwest coast, known for its harbor, Table Mountain, and rich history.",
-    images: "/images/Cape_Town.png",
+    images: "https://portfoliocollection.com/wp-content/uploads/2022/12/kylefromthenorth-hzR9rDXWbqo-unsplash.jpg",
     pointsOfInterest: [
       { name: "Table Mountain", description: "A flat-topped mountain offering panoramic views of the city and surrounding ocean." },
       { name: "Robben Island", description: "A historic site where Nelson Mandela was imprisoned for 18 years." },
@@ -91,7 +91,7 @@ module.exports = [
     city: "Cuzco Machu Picchu",
     country: "Peru",
     description: "A city steeped in history, known as the gateway to the ancient Inca city of Machu Picchu. Cuzco was once the capital of the Inca Empire and is rich in archaeological and cultural sites.",
-    images: "/images/Machu_Picchu.png",
+    images: "https://peruvian-sunrise.com/sites/default/files/2024-09/peruvian-sunrise-travel-agency-south-america-tours-cusco%20%281%29.webp",
     pointsOfInterest: [
       { name: "Machu Picchu", description: "A 15th-century Inca citadel located in the Andes, known for its stunning ancient architecture and breathtaking views." },
       { name: "Sacsayhuamán", description: "A ceremonial complex with impressive stone walls, part of the Inca ruins above Cuzco." },
@@ -102,7 +102,7 @@ module.exports = [
     city: "Huacachina",
     country: "Peru",
     description: "A desert oasis town located in southwestern Peru, famous for its scenic lagoon surrounded by massive sand dunes, offering a unique blend of natural beauty and adventure.",
-    images: "/images/Huacachina.png",
+    images: "https://www.incatrailmachu.com/img/huacachina-02-073.jpg",
     pointsOfInterest: [
       { name: "Huacachina Lagoon", description: "A small oasis surrounded by towering sand dunes, popular for sandboarding and buggy rides." },
       { name: "Sand Dunes", description: "Massive dunes surrounding the oasis, perfect for dune buggy tours and sandboarding." },
@@ -113,7 +113,7 @@ module.exports = [
     city: "Dubai",
     country: "United Arab Emirates",
     description: "A global city known for its ultra-modern architecture, luxury shopping, and vibrant nightlife scene. Dubai is a city of innovation and futuristic design.",
-    images: "/images/Dubai.png",
+    images: "https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/stylized-aerial-view-of-dubai-city-dblight.jpg",
     pointsOfInterest: [
       { name: "Burj Khalifa", description: "The tallest building in the world, offering stunning views of the city from its observation deck." },
       { name: "Palm Jumeirah", description: "An artificial archipelago in the shape of a palm tree, known for its luxury hotels and upscale shopping." },
@@ -124,7 +124,7 @@ module.exports = [
     city: "Barcelona",
     country: "Spain",
     description: "A vibrant city known for its unique mix of modernist architecture, Mediterranean beaches, and cultural landmarks. Barcelona is also the hub of Catalonian culture.",
-    images: "/images/Barcelona.png",
+    images: "https://res.cloudinary.com/dtljonz0f/image/upload/f_auto/q_auto/w_800/v1/gc-v1/barcelona/sagrada_familia-shutterstock_2080319242.jpg",
     pointsOfInterest: [
       { name: "La Sagrada Familia", description: "A famous basilica designed by architect Antoni Gaudí, still under construction after more than 100 years." },
       { name: "Park Güell", description: "A public park designed by Gaudí, featuring colorful mosaics and sweeping views of the city." },
@@ -135,7 +135,7 @@ module.exports = [
     city: "Cappadoccia",
     country: "Turkey",
     description: "A unique region in central Turkey known for its otherworldly landscapes, fairy chimneys, and ancient cave dwellings carved into the soft volcanic rock.",
-    images: "/images/Cappadoccia.png",
+    images: "https://cdn.kimkim.com/files/a/images/7f60455b2302182e8f62b0a25c97dfb6ef4a8c38/big-3aa2ec570920b07c1521217f315e9adb.jpg",
     pointsOfInterest: [
       { name: "Göreme National Park", description: "A UNESCO World Heritage site known for its rock-hewn churches, monasteries, and dwellings." },
       { name: "Hot Air Balloon Ride", description: "A popular activity that offers breathtaking views of the fairy chimneys and the entire Cappadocia region." },
@@ -146,7 +146,7 @@ module.exports = [
     city: "Highlands",
     country: "United Kingdom",
     description: "A rugged and remote region in northern Scotland, famous for its dramatic landscapes, including mountains, lochs, and glens.",
-    images: "/images/Highlands.png",
+    images: "https://www.picturesofscotland.co.uk/wp-content/uploads/2018/02/stag-2431772_1920.jpg",
     pointsOfInterest: [
       { name: "Loch Ness", description: "A large, deep freshwater loch, known for the mythical Loch Ness Monster and its stunning surroundings." },
       { name: "Ben Nevis", description: "The highest mountain in the British Isles, offering challenging climbs and spectacular views of the highlands." },
@@ -157,7 +157,7 @@ module.exports = [
     city: "Iceland",
     country: "Iceland",
     description: "A Nordic island country known for its dramatic landscapes, including volcanoes, glaciers, hot springs, and geysers. Iceland offers some of the most unique natural scenery in the world.",
-    images: "/images/Iceland.png",
+    images: "https://www.campervaniceland.com/assets/img/blog/364.png",
     pointsOfInterest: [
       { name: "Blue Lagoon", description: "A geothermal spa known for its warm, mineral-rich waters and beautiful surrounding lava fields." },
       { name: "Golden Circle", description: "A popular tourist route that includes sites like the Geysir geothermal area, Gullfoss waterfall, and Thingvellir National Park." },
@@ -168,7 +168,7 @@ module.exports = [
     city: "Patagonia",
     country: "Argentina/Chile",
     description: "A remote region at the southern tip of South America, known for its dramatic mountains, glaciers, and diverse wildlife. Patagonia offers some of the best outdoor adventure opportunities.",
-    images: "/images/Patagonia.png",
+    images: "https://media.istockphoto.com/id/1368440238/it/foto/argentina.jpg?s=612x612&w=0&k=20&c=2y9QF4nA7-OLY5S4CnxrOHm6AQ7AEYI-GLjz6Jz2jVM=",
     pointsOfInterest: [
       { name: "Torres del Paine National Park", description: "A stunning national park in Chile, famous for its towering peaks, glaciers, and crystal-clear lakes." },
       { name: "Perito Moreno Glacier", description: "A massive glacier located in Argentine Patagonia, known for its dramatic ice calving." },
